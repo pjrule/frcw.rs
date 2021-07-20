00eff11bb374ffd07ed91991b8bb9447dc267196
Parker J. Rule <parker.rule@tufts.edu>
2021-07-20 02:08:01 -0400

    Suppress balance edge warning

--- src/recom/mod.rs
+++ src/recom/mod.rs
@@ -207,11 +207,14 @@ pub fn random_split(
     if buf.balance_nodes.is_empty() {
         return Err("no balanced cuts".to_string());
     } else if buf.balance_nodes.len() > params.M as usize {
+        // TODO: It might be useful to keep statistics here.
+        /*
         println!(
             "Warning: found {} balance nodes (soft upper bound {})",
             buf.balance_nodes.len(),
             params.M
         );
+        */
     }
     let balance_node = buf.balance_nodes[rng.gen_range(0..buf.balance_nodes.len())];
     buf.deque.push_back(balance_node);
