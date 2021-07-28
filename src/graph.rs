2bdffeccc9cfbdf71af3677118c84e24757e52d7
Parker J. Rule <parker.rule@tufts.edu>
2021-07-28 02:14:55 -0400

    Add rectangular grid generator

--- src/graph.rs
+++ src/graph.rs
@@ -2,7 +2,7 @@
 use std::collections::HashMap;
 
 /// Edges are pairs of node indices.
-#[derive(Clone, Hash, Eq, PartialEq)]
+#[derive(Clone, Hash, Eq, PartialEq, Debug)]
 pub struct Edge(pub usize, pub usize);
 
 /// A lightweight graph with population metadata.
@@ -31,14 +31,57 @@ impl Graph {
     /// Returns a new graph with preallocated containers for `n` nodes and
     /// `8 * n` edges.
     pub fn new_buffer(n: usize) -> Graph {
-        return Graph {
+        Graph {
             pops: Vec::<u32>::with_capacity(n),
             neighbors: vec![Vec::<usize>::with_capacity(8); n],
             edges: Vec::<Edge>::with_capacity(8 * n),
             edges_start: vec![0 as usize; n],
             total_pop: 0,
             attr: HashMap::new(),
-        };
+        }
+    }
+
+    /// Returns an nXm rectangular grid graph with unit population at each node.
+    /// (Useful for testing.)
+    pub fn rect_grid(n: usize, m: usize) -> Graph {
+        let size = n * m;
+        let mut neighbors = Vec::<Vec<usize>>::with_capacity(size);
+        let mut edges = Vec::<Edge>::with_capacity(2 * size);
+        let mut edges_start = Vec::<usize>::with_capacity(size);
+        for col in 0..n {
+            for row in 0..m {
+                edges_start.push(edges.len());
+                let idx = (col * m) + row;
+                let mut node_neighbors = Vec::<usize>::with_capacity(4);
+                if col > 0 {
+                    let west_idx = ((col - 1) * m) + row;
+                    node_neighbors.push(west_idx);
+                }
+                if row > 0 {
+                    let south_idx = (col * m) + (row - 1);
+                    node_neighbors.push(south_idx);
+                }
+                if row < m - 1 {
+                    let north_idx = (col * m) + (row + 1);
+                    node_neighbors.push(north_idx);
+                    edges.push(Edge(idx, north_idx));
+                }
+                if col < n - 1 {
+                    let east_idx = ((col + 1) * m) + row;
+                    node_neighbors.push(east_idx);
+                    edges.push(Edge(idx, east_idx));
+                }
+                neighbors.push(node_neighbors);
+            }
+        }
+        Graph {
+            pops: vec![1 as u32; size],
+            neighbors: neighbors,
+            edges: edges,
+            edges_start: edges_start,
+            total_pop: size as u32,
+            attr: HashMap::new(),
+        }
     }
 
     /// Resets a graph's containers.
@@ -55,5 +98,80 @@ impl Graph {
         // thus, we leave them in for now.
         self.edges_start.fill(0);
         self.total_pop = 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rect_grid_1x1() {
+        let grid = Graph::rect_grid(1, 1);
+        assert_eq!(grid.edges.len(), 0);
+        assert_eq!(grid.pops, vec![1 as u32]);
+        assert_eq!(grid.neighbors, vec![vec![0 as usize; 0]]);
+        assert_eq!(grid.edges_start, vec![0]);
+        assert_eq!(grid.total_pop, 1);
+        assert_eq!(grid.attr.len(), 0);
+    }
+
+    #[test]
+    fn test_rect_grid_2x2() {
+        /*
+         * 1 - 3
+         * |   |
+         * 0 - 2
+         */
+        let grid = Graph::rect_grid(2, 2);
+        assert_eq!(
+            grid.edges,
+            vec![Edge(0, 1), Edge(0, 2), Edge(1, 3), Edge(2, 3)]
+        );
+        assert_eq!(grid.pops, vec![1 as u32; 4]);
+        assert_eq!(
+            grid.neighbors,
+            vec![vec![1, 2], vec![0, 3], vec![0, 3], vec![1, 2]]
+        );
+        assert_eq!(grid.edges_start, vec![0, 2, 3, 4]);
+        assert_eq!(grid.total_pop, 4);
+        assert_eq!(grid.attr.len(), 0);
+    }
+
+    #[test]
+    fn test_rect_grid_3x2() {
+        /*
+         * 1 - 3 - 5
+         * |   |   |
+         * 0 - 2 - 4
+         */
+        let grid = Graph::rect_grid(3, 2);
+        assert_eq!(
+            grid.edges,
+            vec![
+                Edge(0, 1),
+                Edge(0, 2),
+                Edge(1, 3),
+                Edge(2, 3),
+                Edge(2, 4),
+                Edge(3, 5),
+                Edge(4, 5)
+            ]
+        );
+        assert_eq!(grid.pops, vec![1 as u32; 6]);
+        assert_eq!(
+            grid.neighbors,
+            vec![
+                vec![1, 2],
+                vec![0, 3],
+                vec![0, 3, 4],
+                vec![1, 2, 5],
+                vec![2, 5],
+                vec![3, 4]
+            ]
+        );
+        assert_eq!(grid.edges_start, vec![0, 2, 3, 5, 6, 7]);
+        assert_eq!(grid.total_pop, 6);
+        assert_eq!(grid.attr.len(), 0);
     }
 }
