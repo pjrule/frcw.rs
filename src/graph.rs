ed9af7764cf9a0880b7809f72404a90be522a16e
Michael Sarahan <msarahan@gmail.com>
2022-01-09 19:24:36 -0600

    proof of concept python extension for frcw

--- src/graph.rs
+++ src/graph.rs
@@ -1,33 +1,70 @@
 //! A lightweight graph with population metadata.
 use std::cmp::{max, min};
 use std::collections::HashMap;
+use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+use snafu::prelude::*;
+
 
 /// Edges are pairs of node indices.
+#[pyclass]
 #[derive(Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd, Debug)]
 pub struct Edge(pub usize, pub usize);
 
+#[derive(Debug, Snafu)]
+pub enum GraphError {
+    #[snafu(display("Empty edge list"))]
+    ErrEmptyEdgeList,
+    #[snafu(display("Could not parse edge index: {edge_index}"))]
+    ErrEdgeIndexParse { edge_index: String},
+    #[snafu(display("Invalid line in edge list: {line}"))]
+    ErrEdgeListLine { line: String},
+    #[snafu(display("Edges must be 0-indexed or 1-indexed, but minimum index is {value}"))]
+    ErrEdgeIndexMinimumValue {value: usize},
+    #[snafu(display("Duplicate edge: {e0} {e1}"))]
+    ErrDuplicateEdge { e0: usize, e1: usize },
+    #[snafu(display("Mismatch: edge list has {edge_list_nodes} nodes, population list has {pop_list_nodes} nodes"))]
+    ErrNodeLengthMismatch {edge_list_nodes: usize, pop_list_nodes: usize},
+    #[snafu(display("Could not parse population value: {pop}"))]
+    ErrPopulationParse {pop: String },
+}
+
+impl std::convert::From<GraphError> for PyErr {
+    fn from(err: GraphError) -> PyErr {
+        PyValueError::new_err(err.to_string())
+    }
+}
+
 /// A lightweight graph with population metadata.
+#[pyclass]
 #[derive(Clone)]
 pub struct Graph {
     /// The graph's edges, represented as pairs of node indices,
     /// sorted by the first element of the pair.
     /// (Nodes are represented implicitly.)
+    #[pyo3(get, set)]
     pub edges: Vec<Edge>,
     /// The population at each node.
+    #[pyo3(get, set)]
     pub pops: Vec<u32>,
     /// The graph's adjacencies (list-of-lists format).
+    #[pyo3(get, set)]
     pub neighbors: Vec<Vec<usize>>,
     /// Maps between node indices and blocks of edges in `edges`.
     /// The nth element corresponds to the starting index of the
     /// block of edges in `edges` of the form (n, *).
+    #[pyo3(get, set)]
     pub edges_start: Vec<usize>,
     /// The total population over all nodes.
     /// (Should be equal to the sum of `pops`.)
+    #[pyo3(get, set)]
     pub total_pop: u32,
     /// Additional node attributes (optional).
+    #[pyo3(get, set)]
     pub attr: HashMap<String, Vec<u32>>,
 }
 
+// functions/methods implemented only for rust
 impl Graph {
     /// Returns a new graph with preallocated containers for `n` nodes and
     /// `8 * n` edges.
@@ -40,109 +77,6 @@ impl Graph {
             total_pop: 0,
             attr: HashMap::new(),
         }
-    }
-
-    /// Initializes a graph from a newline-delimited edge list format representation.
-    ///
-    /// Arguments:
-    ///   * `edge_list`: The list of edges in the graph. Each line contains
-    ///     two indices delimited by a space. Nodes are assumed to be labeled
-    ///     0..n-1 or 1..n. No edge ordering is assumed.
-    ///   * `populations`: The integer population of each node in the graph,
-    ///     in index order.
-    ///
-    /// If the edge list or populations cannot be parsed, or if the number
-    /// of nodes is inconsistent between `edge_lists` and `populations`,
-    /// and `Err` is returned. Empty graphs are considered invalid, as are
-    /// graphs with duplicate edges.
-    ///
-    /// The caller is responsible for ensuring the graph is connected
-    /// (if that property is desired).
-    pub fn from_edge_list(edge_list: &str, populations: &str) -> Result<Graph, String> {
-        let mut edges = Vec::<Edge>::new();
-        if edge_list.is_empty() {
-            return Err("Empty edge list".to_string());
-        }
-        for line in edge_list.split('\n') {
-            if line.is_empty() {
-                continue;
-            }
-            let indices: Vec<&str> = line.split(' ').collect();
-            if indices.len() != 2 {
-                return Err(format!("Invalid line in edge list: {}", line));
-            }
-            let src = match indices[0].parse::<usize>() {
-                Ok(idx) => idx,
-                Err(_) => return Err(format!("Could not parse edge index: {}", indices[0])),
-            };
-            let dst = match indices[1].parse::<usize>() {
-                Ok(idx) => idx,
-                Err(_) => return Err(format!("Could not parse edge index: {}", indices[1])),
-            };
-            edges.push(Edge(min(src, dst), max(src, dst)));
-        }
-        let min_index = edges.iter().map(|e| e.0).min().unwrap();
-        let max_index = edges.iter().map(|e| e.1).max().unwrap();
-        let n;
-        if min_index == 0 {
-            n = max_index + 1;
-        } else if min_index == 1 {
-            // Fix 1-indexing.
-            n = max_index;
-            edges = edges.iter().map(|e| Edge(e.0 - 1, e.1 - 1)).collect();
-        } else {
-            return Err(format!(
-                "Edges must be 0-indexed or 1-indexed, but minimum index is {}",
-                min_index
-            ));
-        }
-        edges.sort();
-        let mut edges_start = Vec::<usize>::new();
-        let mut neighbors = vec![Vec::<usize>::new(); n];
-        let mut src = edges[0].0;
-        edges_start.push(0);
-        for (idx, edge) in edges.iter().enumerate() {
-            if edge.0 != src {
-                // Handle implicit nodes with no edges.
-                for _ in src..edge.0 {
-                    edges_start.push(idx);
-                }
-                src = edge.0;
-            }
-            if neighbors[edge.0].contains(&edge.1) {
-                return Err(format!("Duplicate edge: {} {}", edge.0 + 1, edge.1 + 1));
-            }
-            neighbors[edge.0].push(edge.1);
-            neighbors[edge.1].push(edge.0);
-        }
-        // Handle implicit nodes with no edges.
-        for _ in src..n - 1 {
-            edges_start.push(edges.len());
-        }
-
-        let mut parsed_pops = Vec::<u32>::with_capacity(neighbors.len());
-        for pop in populations.replace('\n', "").split(' ') {
-            match pop.parse::<u32>() {
-                Ok(parsed) => parsed_pops.push(parsed),
-                Err(_) => return Err(format!("Could not parse population value: {}", pop)),
-            }
-        }
-        if parsed_pops.len() != neighbors.len() {
-            return Err(format!(
-                "Mismatch: edge list has {} nodes, population list has {} nodes",
-                neighbors.len(),
-                parsed_pops.len()
-            ));
-        }
-
-        Ok(Graph {
-            total_pop: parsed_pops.iter().sum(),
-            pops: parsed_pops,
-            neighbors: neighbors,
-            edges: edges,
-            edges_start: edges_start,
-            attr: HashMap::new(),
-        })
     }
 
     /// Returns an nXm rectangular grid graph with unit population at each node.
@@ -188,6 +122,121 @@ impl Graph {
         }
     }
 
+    /// Initializes a graph from a newline-delimited edge list format representation.
+    ///
+    /// Arguments:
+    ///   * `edge_list`: The list of edges in the graph. Each line contains
+    ///     two indices delimited by a space. Nodes are assumed to be labeled
+    ///     0..n-1 or 1..n. No edge ordering is assumed.
+    ///   * `populations`: The integer population of each node in the graph,
+    ///     in index order.
+    ///
+    /// If the edge list or populations cannot be parsed, or if the number
+    /// of nodes is inconsistent between `edge_lists` and `populations`,
+    /// and `Err` is returned. Empty graphs are considered invalid, as are
+    /// graphs with duplicate edges.
+    ///
+    /// The caller is responsible for ensuring the graph is connected
+    /// (if that property is desired).
+    pub fn from_edge_list(edge_list: &str, populations: &str) -> Result<Graph, GraphError> {
+        let mut edges = Vec::<Edge>::new();
+        if edge_list.is_empty() {
+            return Err(GraphError::ErrEmptyEdgeList);
+        }
+        for line in edge_list.split('\n') {
+            if line.is_empty() {
+                continue;
+            }
+            let indices: Vec<&str> = line.split(' ').collect();
+            if indices.len() != 2 {
+                return Err(GraphError::ErrEdgeListLine {line: line.into()});
+            }
+            let src = match indices[0].parse::<usize>() {
+                Ok(idx) => idx,
+                Err(_) => return Err(GraphError::ErrEdgeIndexParse {edge_index: indices[0].into()}),
+            };
+            let dst = match indices[1].parse::<usize>() {
+                Ok(idx) => idx,
+                Err(_) => return Err(GraphError::ErrEdgeIndexParse {edge_index: indices[1].into()}),
+            };
+            edges.push(Edge(min(src, dst), max(src, dst)));
+        }
+        let min_index = edges.iter().map(|e| e.0).min().unwrap();
+        let max_index = edges.iter().map(|e| e.1).max().unwrap();
+        let n;
+        if min_index == 0 {
+            n = max_index + 1;
+        } else if min_index == 1 {
+            // Fix 1-indexing.
+            n = max_index;
+            edges = edges.iter().map(|e| Edge(e.0 - 1, e.1 - 1)).collect();
+        } else {
+            return Err(GraphError::ErrEdgeIndexMinimumValue {value: min_index});
+        }
+        edges.sort();
+        let mut edges_start = Vec::<usize>::new();
+        let mut neighbors = vec![Vec::<usize>::new(); n];
+        let mut src = edges[0].0;
+        edges_start.push(0);
+        for (idx, edge) in edges.iter().enumerate() {
+            if edge.0 != src {
+                // Handle implicit nodes with no edges.
+                for _ in src..edge.0 {
+                    edges_start.push(idx);
+                }
+                src = edge.0;
+            }
+            if neighbors[edge.0].contains(&edge.1) {
+                return Err(GraphError::ErrDuplicateEdge {e0: edge.0 + 1, e1: edge.1 + 1} );
+            }
+            neighbors[edge.0].push(edge.1);
+            neighbors[edge.1].push(edge.0);
+        }
+        // Handle implicit nodes with no edges.
+        for _ in src..n - 1 {
+            edges_start.push(edges.len());
+        }
+
+        let mut parsed_pops = Vec::<u32>::with_capacity(neighbors.len());
+        for pop in populations.replace('\n', "").split(' ') {
+            match pop.parse::<u32>() {
+                Ok(parsed) => parsed_pops.push(parsed),
+                Err(_) => return Err(GraphError::ErrPopulationParse {pop: pop.into()}),
+            }
+        }
+        if parsed_pops.len() != neighbors.len() {
+            return Err(GraphError::ErrNodeLengthMismatch {edge_list_nodes: neighbors.len(), pop_list_nodes: parsed_pops.len()});
+        }
+
+        Ok(Graph {
+            total_pop: parsed_pops.iter().sum(),
+            pops: parsed_pops,
+            neighbors: neighbors,
+            edges: edges,
+            edges_start: edges_start,
+            attr: HashMap::new(),
+        })
+    }
+}
+
+// functions/methods implemented for rust and for Python
+#[pymethods]
+impl Graph {
+    // This is the Python __new__ method for the wrapped class
+    # [new]
+    fn new (n: usize) -> PyResult<Self> {
+        Ok(Graph::new_buffer(n))
+    }
+
+    #[staticmethod]
+    #[args(name="from_edge_list")]
+    fn from_edge_list_py(edge_list: &str, populations: &str) -> PyResult<Self> {
+        return match Graph::from_edge_list(edge_list, populations) {
+            Ok(parsed_graph) => Ok(parsed_graph),
+            Err(graph_error) => Err(graph_error.into()),
+        }
+    }
+
     /// Resets a graph's containers.
     /// (Useful when using a graph as a subgraph buffer.)
     pub fn clear(&mut self) {
@@ -203,6 +252,12 @@ impl Graph {
         self.edges_start.fill(0);
         self.total_pop = 0;
     }
+}
+
+pub(crate) fn init_submodule (module: &PyModule) -> PyResult<()>  {
+    module.add_class::<Edge>()?;
+    module.add_class::<Graph>()?;
+    Ok(())
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-//! Library definition for frcw.
+use pyo3::prelude::*;
+
+/// Library definition for frcw. This is for usage by rust (such as src/bin files)
 mod buffers;
 pub mod config;
 pub mod graph;
@@ -7,3 +9,12 @@ pub mod partition;
 pub mod recom;
 mod spanning_tree;
 pub mod stats;
+
+/// This is collecting all of our wrapped functions to be exposed in the python module
+#[pymodule]
+fn frcw(py: Python, module: &PyModule) -> PyResult<()> {
+    // add graph to the top-level module
+    graph::init_submodule(module)?;
+
+    Ok(())
+}
